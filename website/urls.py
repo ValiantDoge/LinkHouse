b1388384d3ed84f76385d618d370e082a5671ad1
ValiantDoge <agnelofernandes475@gmail.com>
2024-01-18 23:02:38 +0530

    Maybe this will fix

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,13 @@
 from website import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile/<slug:profile_slug>', views.profile, name="profile")
 ]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
