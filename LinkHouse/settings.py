2041c1e8a717d6c886fd06c741e5af07abe3a456
ValiantDoge <agnelofernandes475@gmail.com>
2024-01-18 23:46:50 +0530

    env hide

--- LinkHouse/settings.py
+++ LinkHouse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zb^tw_o412sr!yr7s*lh!6g_h1!axfm3n1@l!q6$-mj67i-9ly'
+SECRET_KEY = os.environ.get("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
